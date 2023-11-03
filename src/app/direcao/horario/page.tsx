5f11c596fdbc6e33ac744aa43f392098511cafff
Your Name <caihebatista@gmail.com>
2023-11-03 20:57:56 -0300

    fetch changes

--- src/app/direcao/horario/page.tsx
+++ src/app/direcao/horario/page.tsx
@@ -1,18 +1,30 @@
+"use client"
 import { URL } from '@/util/consts'
 import axios, { AxiosPromise } from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Horarios } from './Horario'
+import { Tempo } from '@/util/entities/tempo'
 
-export default async function page({}: {}) {
-    const promises = [] as AxiosPromise[]
-    const t = ['1001', '1002','1003','1004','2001','2002','2003','2004','3001','3002','3003','3004']
-    t.forEach(turma => {
-        promises.push(axios(URL+'horario/'+turma))
-    })
-    const data = (await Promise.all(promises)).map(res => res.data)
+export default function page({}: {}) {
+    const [data, setData] = useState<null | Tempo[][][]>(null)
+    useEffect(() => {
+        const promises = [] as AxiosPromise[]
+        const t = ['1001', '1002','1003','1004','2001','2002','2003','2004','3001','3002','3003','3004']
+        t.forEach(turma => {
+            promises.push(axios(URL+'horario/'+turma))
+        })
+        const data = Promise.all(promises).then(data => setData(data.map(res => res.data)))
+    }, [])
     return (
         <>
-            <Horarios data={(data ?? [])}/>
+            {data ? 
+            (<>
+                <Horarios data={data}/>
+            </>) 
+            : (<>
+                <h1>Carregando...</h1>
+                <h6>Pode demorar um pouco...</h6>
+            </>)}
         </>
     )
 }
